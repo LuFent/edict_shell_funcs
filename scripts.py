3fd2b2b103c0b22da72555cb294227f8fdebd727
LuFent <vladmarchenkosteam@gmail.com>
2022-01-27 16:42:11 +0300

    Exception catching added

--- scripts.py
+++ scripts.py
@@ -1,40 +1,72 @@
-"""
+
 from datacenter.models import Mark
 from datacenter.models import Schoolkid
-"""
-def fix_marks(schoolkid):
+
+
+def fix_marks(schoolkid_name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except Schoolkid.DoesNotExist:
+        print("Ученик с таким именем не найдем")
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print("Уточните имя")
+        return
     min_good_mark = 4
     bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__lt=min_good_mark)
     for mark in bad_marks:
         mark.points = 5
         mark.save()
+    print("Оценки исправлены")
 
 
-"""
+
+
 from datacenter.models import Chastisement
 from datacenter.models import Schoolkid
-"""
-def delete_chastisement(schoolkid):
+
+
+def delete_chastisement(schoolkid_name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except Schoolkid.DoesNotExist:
+        print("Ученик с таким именем не найдем")
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print("Уточните имя")
+        return
     chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
     for chastisement in chastisements:
         chastisement.delete()
 
 
-"""
+
 from datacenter.models import Commendation
 from datacenter.models import Schoolkid
 from datacenter.models import Lesson
 from datacenter.models import Subject
 from random import choice
-"""
+
+
 def create_commendation(schoolkid_name, subject_name):
     possible_commends = ["Молодец!", "Отлично!", "Хорошо!", "Очень хороший ответ!", "Замечательно!", "Так держать!"]
     commend = choice(possible_commends)
-    schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except Schoolkid.DoesNotExist:
+        print("Ученик с таким именем не найдем")
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print("Уточните имя")
+        return
     year = schoolkid.year_of_study
     letter = schoolkid.group_letter
-    subject = Subject.objects.get(title=subject_name, year_of_study=year)
+    try:
+        subject = Subject.objects.get(title=subject_name, year_of_study=year)
+    except Subject.DoesNotExist:
+        print("Такого предмета не найдено")
+        return
     lessons = Lesson.objects.filter(subject=subject,  year_of_study=year, group_letter=letter).order_by("date")
     last_lesson = lessons.last()
     Commendation.objects.create(text=commend, created=last_lesson.date, schoolkid=schoolkid, subject=subject,
-                                teacher=some_lesson.teacher)
+                                teacher=last_lesson.teacher)
